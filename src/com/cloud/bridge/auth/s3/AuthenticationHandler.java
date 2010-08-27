7c33abd53d9609114da20df1ccbb1081b9e6a114
Mark Joseph <mark@cloud.com>
2010-08-27 11:12:36 -0700

    Turning the S3 authentication tests back on

--- src/com/cloud/bridge/auth/s3/AuthenticationHandler.java
+++ src/com/cloud/bridge/auth/s3/AuthenticationHandler.java
@@ -149,7 +149,7 @@ public class AuthenticationHandler implements Handler {
     	}
     	
         // -> for SOAP requests the Cloud API keys are sent here and only here
-    	//S3SoapAuth.verifySignature( msgSig, operation, timestamp, accessKey, secretKey );   	
+    	S3SoapAuth.verifySignature( msgSig, operation, timestamp, accessKey, secretKey );   	
         UserContext.current().initContext( accessKey, secretKey, accessKey, "S3 SOAP request" );
         return InvocationResponse.CONTINUE;
      }
