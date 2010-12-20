a4a50ad93687e56ab0ccebbde0c116122c63d363
mark <mark@p6r.com>
2010-12-19 20:23:55 -0800

    To support multipart uploads we need to keep the state of the uploads in the database.

--- db/mysql/cloudbridge_index.sql
+++ db/mysql/cloudbridge_index.sql
@@ -45,3 +45,8 @@ ALTER TABLE mhost_mount ADD CONSTRAINT FOREIGN KEY mhost_mnt_fk_shost_id(SHostID
 ALTER TABLE mhost_mount ADD INDEX mhost_mnt_idx_mhost_id(MHostID);
 ALTER TABLE mhost_mount ADD INDEX mhost_mnt_idx_shost_id(SHostID);
 ALTER TABLE mhost_mount ADD INDEX mhost_mnt_idx_mount_time(LastMountTime);
+
+ALTER TABLE multipart_meta ADD CONSTRAINT FOREIGN KEY meta_uploads_id(UploadID) REFERENCES multipart_uploads(ID) ON DELETE CASCADE;
+ALTER TABLE multipart_parts ADD CONSTRAINT FOREIGN KEY part_uploads_id(UploadID) REFERENCES multipart_uploads(ID) ON DELETE CASCADE;
+ALTER TABLE multipart_parts ADD UNIQUE part_uploads_number(UploadId, partNumber);
+
