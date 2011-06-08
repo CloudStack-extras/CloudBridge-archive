91e97e975a2933ae73f466e35cead08e649eb347
Kelven Yang <kelven@cloud.com>
2011-06-07 18:25:31 -0700

    Forgot to add new file, here it is

---
+++ src/com/cloud/stack/CloudStackResourceLimit.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2010 Cloud.com, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cloud.stack;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CloudStackResourceLimit {
+    @SerializedName("account")
+    private String accountName;
+
+    @SerializedName("domainid")
+    private Long domainId;
+
+    @SerializedName("domain")
+    private String domainName;
+
+    @SerializedName("resourcetype")
+    private String resourceType;
+
+    @SerializedName("max")
+    private Long max;
+
+    public CloudStackResourceLimit() {
+    }
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public Long getDomainId() {
+		return domainId;
+	}
+
+	public String getDomainName() {
+		return domainName;
+	}
+
+	public String getResourceType() {
+		return resourceType;
+	}
+
+	public Long getMax() {
+		return max;
+	}
+}
